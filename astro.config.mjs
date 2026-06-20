d92fb6c75c52697e75e042a4f877422faddc066a
Arjun Ingole <arjuningole38@gmail.com>
2026-06-20 20:47:21 +0530

    [add] deployment configs for github pages
    Signed-off-by: Arjun Ingole <arjuningole38@gmail.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://arjun-ingole.github.io',
   markdown: {
     shikiConfig: { theme: 'houston' }
   },
